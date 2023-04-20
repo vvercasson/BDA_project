bdfb7d41ead020e5b1008c44367c007120145688
Victor Vercasson <victor.vercasson@etu.u-bordeaux.fr>
2023-04-20 22:12:37 +0200

    a

--- creationScripts/copyToTables.py
+++ creationScripts/copyToTables.py
@@ -31,9 +31,16 @@ statsAnneeTable = 'statscomannee'
 statsInterTable = 'statscominter'
 
 # Connection to my database
-conn = connect()
+print('Connexion à la base de données...')
+USERNAME=input("Saisir le nom d'utilisateur de la base de données : ")
+PASS= getpass.getpass('Mot de passe de '+ USERNAME + ':')
 
-print('Successfull connection')
+try:
+    conn = psycopg2.connect("host=pgsql dbname="+ USERNAME+" user="+ USERNAME+ " password="+PASS)
+except Exception as e :
+    exit("Connexion impossible à la base de données: " + str(e))
+
+print('Connecté à la base de données')
 
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
