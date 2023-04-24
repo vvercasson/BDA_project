76eb1a03bf13c009f1b0601af4bb7a8e45f31ce7
Martin Ithurbide <martin.ithurbide@etu.u-bordeaux.fr>
2023-04-24 17:55:28 +0200

    trigger update

--- SQL_Files/Triggers/UPDATE_POP_TRIGGER.sql
+++ SQL_Files/Triggers/UPDATE_POP_TRIGGER.sql
@@ -1,14 +1,27 @@
 CREATE OR REPLACE FUNCTION refresh_pop_on_both_tables()
 RETURNS TRIGGER AS $$
-BEGIN
-    CALL update_reg_pop19();
-    CALL update_dept_pop19();
-    RETURN NEW;
-END;
+        DECLARE totalNewPop INTEGER;
+        DECLARE totalPrevPop INTEGER;
+        BEGIN
+            IF NOT EXISTS(SELECT idcom FROM statscomannee where idstat = new.idstat and valeur is null) THEN
+                totalNewPop := (SELECT COUNT(idstat)
+                FROM statscomannee
+                WHERE idstat = new.idstat);
+
+                totalPrevPop := (SELECT COUNT(idstat)
+                FROM statscomannee
+                WHERE idstat = 'P13_POP');
+
+                IF (totalNewPop = totalPrevPop) THEN
+                    CALL update_dept_pop19();
+                    CALL update_reg_pop19();
+                END IF;
+            END IF;
+            RETURN NEW;
+        END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_population_on_statsAnnee_update
-AFTER UPDATE ON STATSCOMANNEE
+CREATE TRIGGER update_population_on_statsAnnee_update AFTER UPDATE OR INSERT ON STATSCOMANNEE
 FOR EACH ROW
-WHEN (OLD.valeur IS DISTINCT FROM NEW.valeur AND OLD.IDSTAT LIKE '%POP%')
+WHEN (new.idstat LIKE '%POP%')
 EXECUTE FUNCTION refresh_pop_on_both_tables();
